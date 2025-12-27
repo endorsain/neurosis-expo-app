3957f3573a0ae0d6f915d1b397a2f674fc82fff3
endorsain <blanco.genaro.mauro@gmail.com>
2025-12-27 17:53:39 -0300

    Modificacion de iconos por defecto

--- App.tsx
+++ App.tsx
@@ -1,11 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your!</Text>
-      <Text>no mameSSSS</Text>
+      <Text>Hola Neurosis!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +13,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#b49898ff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#b49898ff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
